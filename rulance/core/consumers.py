34e5d04b7654ccb87a72fe8a44cb0720345d0144
payzinn <crazyff23124@gmail.com>
2025-04-29 14:43:15 +0600

    date in chat

--- rulance/core/consumers.py
+++ rulance/core/consumers.py
@@ -5,25 +5,23 @@ from .models import Chat, Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.chat_id   = self.scope["url_route"]["kwargs"]["chat_id"]
+        self.chat_id = self.scope["url_route"]["kwargs"]["chat_id"]
         self.group_name = f"chat_{self.chat_id}"
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
-        print(f"[ChatConsumer] CONNECT chat_id={self.chat_id}")
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data):
-        print("[ChatConsumer] receive:", text_data)
         data = json.loads(text_data)
         text = data["message"]
         user = self.scope["user"]
 
         chat = await database_sync_to_async(Chat.objects.get)(pk=self.chat_id)
         msg  = await database_sync_to_async(Message.objects.create)(
-                    chat=chat, sender=user, text=text
-               )
+            chat=chat, sender=user, text=text
+        )
 
         await self.channel_layer.group_send(
             self.group_name,
@@ -31,10 +29,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 "type": "chat.message",
                 "message": text,
                 "sender": user.username,
-                "sender_id": user.id, 
+                "sender_id": user.id,
                 "avatar_url": user.avatar.url or "",
                 "sender_full_name": user.full_name or user.username,
                 "time": msg.timestamp.strftime("%H:%M"),
+                "date": msg.timestamp.strftime("%Y-%m-%d"),
+                "date_readable": msg.timestamp.strftime("%d.%m.%Y"),
             }
         )
 
