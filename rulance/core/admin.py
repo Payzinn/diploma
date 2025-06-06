87dbabae2c42ae698c33f80bb9c1784ad864e02a
payzinn <crazyff23124@gmail.com>
2025-06-06 17:11:31 +0600

    admin fix

--- rulance/core/admin.py
+++ rulance/core/admin.py
@@ -2,12 +2,8 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(User)
-admin.site.register(Payment)
 admin.site.register(Sphere)
 admin.site.register(SphereType)
 admin.site.register(Order)
 admin.site.register(Portfolio)
 admin.site.register(Response)
-admin.site.register(Notification)
-admin.site.register(Chat)
-admin.site.register(Message)
